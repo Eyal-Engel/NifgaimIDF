f3bb1f7bbd024c06590370010e0f192854ceeacc
Eyal-Engel <eyalengel13@gmail.com>
2024-03-06 09:09:56 +0200

    ADD JSON ON COTRNLLER CREATE HALAL

--- backend/controllers/halalsCotnrollers.js
+++ backend/controllers/halalsCotnrollers.js
@@ -777,6 +777,27 @@ const getHalalsByCommandId = async (req, res, next) => {
     return next(err);
   }
 };
+
+// {
+//   "privateNumber": "1010101",
+//   "lastName": "Doe",
+//   "firstName": "John",
+//   "dateOfDeath": "2024-03-03",
+//   "serviceType": "קבע",
+//   "circumstances": "Combat",
+//   "unit": "Alpha Company",
+//   "division": "1st Division",
+//   "specialCommunity": "Veterans",
+//   "area": "Section A",
+//   "plot": "Plot 123",
+//   "line": "Line 1",
+//   "graveNumber": "456",
+//   "permanentRelationship": false,
+//   "comments": "Lorem ipsum dolor sit.",
+//   "nifgaimCommandId": "07ec94ec-d900-4633-8c40-b47f25ac6a9c",
+//   "nifgaimGraveyardId": "286ad23d-450c-47c4-b23d-377ac18b993b",
+//   "testing": "testy"
+// }
 const createHalal = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
